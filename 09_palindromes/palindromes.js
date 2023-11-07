5cf99c967b89e64f7dd80bcde13a306d5446392d
m-malinowski <mm@oxionics.com>
2023-11-07 09:21:50 +0100

    solved exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+function removePunctuation(text) {
+    var punctuation = /[\.,?! ]/g;
+    var newText = text.replace(punctuation, "");
+    newText = newText.toLowerCase();
+    return newText;
+  }
 
+const palindromes = function (string) {
+    string = removePunctuation(string)
+    const arr = string.split('')
+    arr.reverse()
+    stringReversed = arr.join('')
+    console.log(string, stringReversed)
+    return (string == stringReversed)
 };
 
 // Do not edit below this line
