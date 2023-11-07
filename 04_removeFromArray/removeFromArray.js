f79e1707a1b821b19690d559b118f71e755af410
m-malinowski <mm@oxionics.com>
2023-11-07 08:48:47 +0100

    actually solved exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    const valsToRemove = theArgs;
+    for (let i=0; i < valsToRemove.length; i++){
+        let val = valsToRemove[i]
+        const index = array.indexOf(val);
+        if (index > -1) { // only splice array when item is found
+            array.splice(index, 1); // 2nd parameter means remove one item only
+          }
+    }
+    return array
 };
 
 // Do not edit below this line
