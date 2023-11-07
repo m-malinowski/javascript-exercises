b8808a70c80ffaa476c3252450274efe9c134849
m-malinowski <mm@oxionics.com>
2023-11-07 08:53:01 +0100

    more compact solution to exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,5 @@
-const removeFromArray = function(array, ...theArgs) {
-    const valsToRemove = theArgs;
-    for (let i=0; i < valsToRemove.length; i++){
-        let val = valsToRemove[i]
-        const index = array.indexOf(val);
-        if (index > -1) { // only splice array when item is found
-            array.splice(index, 1); // 2nd parameter means remove one item only
-          }
-    }
-    return array
+const removeFromArray = function(array, ...valsToRemove) {
+    return array.filter(val => !valsToRemove.includes(val));
 };
 
 // Do not edit below this line
