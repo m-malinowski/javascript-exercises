4cc93f0f0e2e6f0196607d78c5af83e10b7a649b
m-malinowski <mm@oxionics.com>
2023-11-07 09:40:24 +0100

    solved final exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = people[0]
+    people.forEach(person => {
+        if (getAge(person) > getAge(oldestPerson)){
+            oldestPerson = person
+        }
+    });
+    return oldestPerson
 };
+
+const getAge = function(person) {
+    const currentYear = 2023
+    const finalYear = ('yearOfDeath' in person? person.yearOfDeath : currentYear);
+    const initYear = person.yearOfBirth;
+    const age = finalYear - initYear
+    return age
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
