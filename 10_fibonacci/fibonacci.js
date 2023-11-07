772ca534bc4f02773fbd685d5136ec1eb3c1a937
m-malinowski <mm@oxionics.com>
2023-11-07 09:25:40 +0100

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 1){
+        return "OOPS"
+    }
+    if (x == 1){
+        return 1
+    }
+    if (x==2){
+        return 1
+    }
+    else{
+        return fibonacci(x-2) + fibonacci(x-1)
+    }
 };
 
 // Do not edit below this line
